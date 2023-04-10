e1bef92f9a54db5b8f2f48bdcf148078f1da4ca4
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-10 07:59:36 +0100

    Add season history to team endpoint

--- server/index.js
+++ server/index.js
@@ -56,10 +56,39 @@ app.get('/player/:playerId', (request, response) => {
 })
 
 app.get('/team/:teamId', (request, response) => {
+  const teamId = request.params.teamId
   const team = teams[request.params.teamId]
 
   if (team) {
-    response.json(team)
+    const resultHistory = Object.keys(tables).filter((seasonId) =>
+      tables[seasonId].some((t) => t.teamId === teamId)
+    )
+
+    const history = []
+
+    const stats = {
+      played: 0,
+      wins: 0,
+      draws: 0,
+      losses: 0,
+      for: 0,
+      against: 0,
+    }
+
+    resultHistory.forEach((seasonId) => {
+      const result = tables[seasonId].find((t) => t.teamId === teamId)
+
+      history.push({ ...result, seasonId, teamId: undefined })
+
+      stats.played += result.played
+      stats.wins += result.wins
+      stats.draws += result.draws
+      stats.losses += result.losses
+      stats.for += result.for
+      stats.against += result.against
+    })
+
+    response.json({ ...team, history, stats })
   } else {
     response.sendStatus(404)
   }
