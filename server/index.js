cfea9159e9919f8db6b20750c140a9d90bc6595d
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-10 18:21:39 +0100

    Remove half implemented squad route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,4 @@
 const express = require('express')
-
-const teams = require('../data/teams.json')
-const squads = require('../data/squads.json')
-const tables = require('../data/tables.json')
-const players = require('../data/players.json')
-
 const { playerRoute, teamRoute, tableRoute, squadRoute } = require('./routes')
 
 const app = express()
@@ -18,35 +12,6 @@ app.get('/player/:playerId', playerRoute)
 app.get('/team/:teamId', teamRoute)
 
 app.get('/table/:seasonId', tableRoute)
-
-app.get('/squad/:teamId', (request, response) => {
-  const team = teams[request.params.teamId]
-
-  if (team) {
-    const seasons = squads
-      .filter((squad) => squad.teamId === request.params.teamId)
-      .map((squad) => squad.seasonId)
-
-    response.json({ seasons })
-  } else {
-    response.sendStatus(404)
-  }
-
-  const squad = squads.find((squad) => {
-    return squad.teamId === request.params.teamId && squad.seasonId === request.params.seasonId
-  })
-
-  if (squad) {
-    const squadData = squad.players.map((p) => {
-      const player = players[p.playerId]
-      return { ...p, ...player }
-    })
-
-    response.json(squadData)
-  } else {
-    response.sendStatus(404)
-  }
-})
 
 app.get('/squad/:teamId/:seasonId', squadRoute)
 
