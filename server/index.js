385cf5ee376bbf4d35d9ede887f58c1c48060980
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-10 14:24:09 +0100

    Abstract player route handler into separate module

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const teams = require('../data/teams.json')
 const squads = require('../data/squads.json')
 const tables = require('../data/tables.json')
 const players = require('../data/players.json')
-const stickers = require('../data/stickers.json')
+
+const { playerRoute } = require('./routes')
 
 const app = express()
 
@@ -12,51 +13,11 @@ app.get('/', (request, response) => {
   response.send('Home page')
 })
 
-app.get('/player/:playerId', (request, response) => {
-  const playerId = request.params.playerId
-  const player = players[playerId]
-
-  if (player) {
-    const squadHistory = squads.filter((squad) =>
-      squad.players.some((p) => p.playerId === playerId)
-    )
-
-    const history = []
-    const stats = { appearances: 0, cleanSheets: 0, goals: 0 }
-
-    squadHistory.forEach((squad) => {
-      const team = teams[squad.teamId]
-      const table = tables[squad.seasonId]
-      const result = table.find((t) => t.teamId === squad.teamId)
-      const member = squad.players.find((p) => p.playerId === playerId)
-
-      history.push({
-        ...team,
-        optaId: undefined,
-        pulseId: undefined,
-        rank: result.rank,
-        seasonId: squad.seasonId,
-        appearances: member.appearances,
-        cleanSheets: member.cleanSheets,
-        goals: member.goals,
-    })
-
-      stats.appearances += member.appearances
-      stats.cleanSheets += member.cleanSheets
-      stats.goals += member.goals
-    })
-
-    const album = stickers[playerId] || null
-
-    response.json({ ...player, history, stats, album })
-  } else {
-    response.sendStatus(404)
-  }
-})
+app.get('/player/:playerId', playerRoute)
 
 app.get('/team/:teamId', (request, response) => {
   const teamId = request.params.teamId
-  const team = teams[request.params.teamId]
+  const team = teams[teamId]
 
   if (team) {
     const resultHistory = Object.keys(tables).filter((seasonId) =>
@@ -77,7 +38,7 @@ app.get('/team/:teamId', (request, response) => {
     resultHistory.forEach((seasonId) => {
       const result = tables[seasonId].find((t) => t.teamId === teamId)
 
-      history.push({ ...result, seasonId, teamId: undefined })
+      history.push({ seasonId, ...result, teamId: undefined })
 
       stats.played += result.played
       stats.wins += result.wins
@@ -103,6 +64,35 @@ app.get('/table/:seasonId', (request, response) => {
     })
 
     response.json(tableData)
+  } else {
+    response.sendStatus(404)
+  }
+})
+
+app.get('/squad/:teamId', (request, response) => {
+  const team = teams[request.params.teamId]
+
+  if (team) {
+    const seasons = squads
+      .filter((squad) => squad.teamId === request.params.teamId)
+      .map((squad) => squad.seasonId)
+
+    response.json({ seasons })
+  } else {
+    response.sendStatus(404)
+  }
+
+  const squad = squads.find((squad) => {
+    return squad.teamId === request.params.teamId && squad.seasonId === request.params.seasonId
+  })
+
+  if (squad) {
+    const squadData = squad.players.map((p) => {
+      const player = players[p.playerId]
+      return { ...p, ...player }
+    })
+
+    response.json(squadData)
   } else {
     response.sendStatus(404)
   }
---
+++ server/routes/playerRoute.js
@@ -0,0 +1,44 @@
+const teams = require('../../data/teams.json')
+const squads = require('../../data/squads.json')
+const tables = require('../../data/tables.json')
+const players = require('../../data/players.json')
+const stickers = require('../../data/laststicker.json')
+
+module.exports = function playerRoute(request, response) {
+  const playerId = request.params.playerId
+  const player = players[playerId]
+
+  if (player) {
+    const squadHistory = squads.filter((squad) =>
+      squad.players.some((p) => p.playerId === playerId)
+    )
+
+    const history = []
+    const stats = { appearances: 0, cleanSheets: 0, goals: 0 }
+
+    squadHistory.forEach((squad) => {
+      const table = tables[squad.seasonId]
+      const result = table.find((t) => t.teamId === squad.teamId)
+      const member = squad.players.find((p) => p.playerId === playerId)
+
+      history.push({
+        seasonId: squad.seasonId,
+        teamId: squad.teamId,
+        rank: result.rank,
+        appearances: member.appearances,
+        cleanSheets: member.cleanSheets,
+        goals: member.goals,
+      })
+
+      stats.appearances += member.appearances
+      stats.cleanSheets += member.cleanSheets
+      stats.goals += member.goals
+    })
+
+    const album = stickers[playerId] || null
+
+    response.json({ ...player, history, stats, album })
+  } else {
+    response.sendStatus(404)
+  }
+}
