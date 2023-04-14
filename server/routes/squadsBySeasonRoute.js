efcac14041387fbc3f195041b8e2835c5429fb8e
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-14 16:29:24 +0100

    Remove use of .reduce() to make squad routes more straightforward to parse

--- server/routes/squadsBySeasonRoute.js
+++ server/routes/squadsBySeasonRoute.js
@@ -4,16 +4,16 @@ const baseUrl = require('../lib/baseUrl')
 module.exports = function squadsBySeasonRoute(request, response) {
   const seasonId = request.params.seasonId
 
-  const squadsData = squads.reduce((acc, squad) => {
+  const squadsData = []
+
+  squads.forEach((squad) => {
     if (squad.seasonId === seasonId) {
       acc.push({
         id: squad.teamId,
         rel: baseUrl(request, 'squads', seasonId, squad.teamId)
       })
     }
-
-    return acc
-  }, [])
+  })
 
   response.json(squadsData)
 }
--- server/routes/squadsRoute.js
+++ server/routes/squadsRoute.js
@@ -2,22 +2,14 @@ const squads = require('../../data/squads.json')
 const baseUrl = require('../lib/baseUrl')
 
 module.exports = function squadsRoute(request, response) {
-  const seen = new Set()
+  const seasonIds = new Set(squads.map((squad) => squad.seasonId))
 
-  const squadsData = squads.reduce((acc, squad) => {
-    const { seasonId } = squad
-
-    if (!seen.has(seasonId)) {
-      seen.add(seasonId)
-
-      acc.push({
-        id: seasonId,
-        rel: baseUrl(request, 'squads', seasonId)
-      })
+  const squadsData = Array.from(seasonIds).map((seasonId) => (
+    {
+      id: seasonId,
+      rel: baseUrl(request, 'squads', seasonId)
     }
-
-    return acc
-  }, [])
+  ))
 
   response.json(squadsData)
 }
