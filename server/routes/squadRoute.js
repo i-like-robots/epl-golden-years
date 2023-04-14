ce8140cca2c9a58adb368a78ebcb1f56ee144edb
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-14 08:49:40 +0100

    Add related links to squad endpoint

--- server/routes/squadRoute.js
+++ server/routes/squadRoute.js
@@ -1,3 +1,5 @@
+const pick = require('../lib/object-pick')
+const restfulUri = require('../lib/baseUrl')
 const squads = require('../../data/squads.json')
 
 module.exports = function squadRoute(request, response) {
@@ -6,7 +8,20 @@ module.exports = function squadRoute(request, response) {
   })
 
   if (squad) {
-    response.json(squad)
+    const players = squad.players.map((player) => (
+      {
+        player: restfulUri(request, 'players', player.playerId),
+        ...pick(player, 'appearances', 'cleanSheets', 'goals'),
+      }
+    ))
+
+    const squadData = {
+      season: restfulUri(request, 'seasons', squad.seasonId),
+      team: restfulUri(request, 'teams', squad.teamId),
+      players
+    }
+
+    response.json(squadData)
   } else {
     response.sendStatus(404)
   }
