f575f70263819cceb3dd6798eb22918c12926a51
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-16 13:44:55 +0100

    Add basic search to players endpoint

---
+++ server/lib/object-matches.js
@@ -0,0 +1,4 @@
+module.exports = function matches(term) {
+  const pattern = new RegExp(`\\b${term}`, 'i')
+  return (obj, ...props) => props.some((prop) => pattern.test(obj[prop]))
+}
--- server/routes/playersRoute.js
+++ server/routes/playersRoute.js
@@ -1,14 +1,23 @@
 const baseUrl = require('../lib/baseUrl')
+const matches = require('../lib/object-matches')
 const players = require('../../data/players.json')
 
 module.exports = function playersRoute(request, response) {
   const playerIds = Object.keys(players)
-  const playersData = playerIds.map((playerId) => (
-    {
-      id: playerId,
-      rel: baseUrl(request, 'players', playerId),
+  const playersData = []
+
+  const matcher = request.query.search ? matches(request.query.search) : null
+
+  playerIds.forEach((playerId) => {
+    const player = players[playerId]
+
+    if (matcher ? matcher(player, 'displayName', 'firstName', 'lastName') : true) {
+      playersData.push({
+        id: playerId,
+        rel: baseUrl(request, 'players', playerId),
+      })
     }
-  ))
+  })
 
   response.json(playersData)
 }
