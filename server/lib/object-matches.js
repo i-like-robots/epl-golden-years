6066aea4239fb2e0668dd585a9d8a6998dfacd19
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-16 21:18:54 +0100

    Validate player name search term before parsing it

--- server/lib/object-matches.js
+++ server/lib/object-matches.js
@@ -1,4 +1,4 @@
 module.exports = function matches(term) {
   const pattern = new RegExp(`\\b${term}`, 'i')
-  return (obj, ...props) => props.some((prop) => pattern.test(obj[prop]))
+  return (obj, props) => props.some((prop) => pattern.test(obj[prop]))
 }
--- server/routes/playersRoute.js
+++ server/routes/playersRoute.js
@@ -2,14 +2,21 @@ const { players } = require('../dataset')
 const { playerUrl } = require('../lib/urls')
 const matches = require('../lib/object-matches')
 
+const SEARCH_PATTERN = /^[a-z]+$/i
+
+const SEARCH_FIELDS = ['displayName', 'firstName', 'lastName']
+
+const isSearchValid = (search) => search && SEARCH_PATTERN.test(search)
+
 module.exports = function playersRoute(request, response) {
-  const matcher = request.query.search ? matches(request.query.search) : null
+  const { search } = request.query
+  const matcher = isSearchValid(search) ? matches(search) : null
   const playersData = []
 
   Object.keys(players).forEach((playerId) => {
     const player = players[playerId]
 
-    if (matcher ? matcher(player, 'displayName', 'firstName', 'lastName') : true) {
+    if (matcher ? matcher(player, SEARCH_FIELDS) : true) {
       playersData.push(playerUrl(playerId))
     }
   })
