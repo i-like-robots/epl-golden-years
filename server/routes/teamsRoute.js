ef6b19e2b9cb49bd3fd562d343a8bee812df52f0
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-17 08:53:59 +0100

    Add basic search to teams resource

--- server/routes/teamsRoute.js
+++ server/routes/teamsRoute.js
@@ -1,8 +1,25 @@
 const { teams } = require('../dataset')
 const { teamUrl } = require('../lib/urls')
+const matches = require('../lib/object-matches')
+
+const SEARCH_PATTERN = /^[a-z]+$/i
+
+const SEARCH_FIELDS = ['name', 'shortName']
+
+const isSearchValid = (search) => search && SEARCH_PATTERN.test(search)
 
 module.exports = function teamsRoute(request, response) {
-  const teamsData = Object.keys(teams).map((teamId) => teamUrl(teamId))
+  const { search } = request.query
+  const matcher = isSearchValid(search) ? matches(search) : null
+  const teamsData = []
+
+  Object.keys(teams).forEach((teamId) => {
+    const team = teams[teamId]
+
+    if (matcher ? matcher(team, SEARCH_FIELDS) : true) {
+      teamsData.push(teamUrl(teamId))
+    }
+  })
 
   response.send(teamsData)
 }
