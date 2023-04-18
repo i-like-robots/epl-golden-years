abab5f82681ffdec2775545619602f47563075b1
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-18 07:45:17 +0100

    refactor hat tricks route to avoid looping twice

--- server/routes/seasonHatTricksRoute.js
+++ server/routes/seasonHatTricksRoute.js
@@ -6,14 +6,18 @@ module.exports = function seasonHatTricksRoute(request, response) {
   const table = tables[seasonId]
 
   if (table) {
-    const data = hatTricks
-      .filter((hatTrick) => hatTrick.seasonId === seasonId)
-      .map((hatTrick) => ({
-        player: playerUrl(hatTrick.playerId),
-        homeTeam: teamUrl(hatTrick.homeTeamId),
-        awayTeam: teamUrl(hatTrick.awayTeamId),
-        date: hatTrick.date,
-      }))
+    const data = []
+
+    hatTricks.forEach((hatTrick) => {
+      if (hatTrick.seasonId === seasonId) {
+        data.push({
+          player: playerUrl(hatTrick.playerId),
+          homeTeam: teamUrl(hatTrick.homeTeamId),
+          awayTeam: teamUrl(hatTrick.awayTeamId),
+          date: hatTrick.date,
+        })
+      }
+    })
 
     response.send({ season: seasonUrl(seasonId), hatTricks: data })
   } else {
