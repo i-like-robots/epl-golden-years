2c272bec96bacda6fa193f07535862fa39fa6016
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-20 23:38:19 +0100

    Fix port reference in startup log message

--- server/start.js
+++ server/start.js
@@ -1,10 +1,12 @@
 const app = require('./app')
 
-app.listen({ port: process.env.PORT || 3000 }, (error) => {
+const port = process.env.PORT || 3000
+
+app.listen({ port }, (error) => {
   if (error) {
     app.log.error(error)
     process.exit(1)
   }
 
-  app.log.info('App is listening at http://localhost:3000')
+  app.log.info(`App is listening at http://localhost:${port}`)
 })
