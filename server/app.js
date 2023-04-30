1ac96c5c40e34ad357f4e7defd5db7e5fe83f52e
Matt Hinchliffe <matt@maketea.co.uk>
2023-04-30 09:08:26 +0100

    Asbtract API route definitions into separate module

--- server/app.js
+++ server/app.js
@@ -1,41 +1,6 @@
 const fastify = require('fastify')
 const swagger = require('@fastify/swagger')
-const {
-  playerRoute,
-  playersRoute,
-  playerAlbumRoute,
-  playerStatsRoute,
-  seasonsRoute,
-  seasonRoute,
-  seasonHatTricksRoute,
-  seasonTableRoute,
-  seasonTopAssistsRoute,
-  seasonTopScorersRoute,
-  teamRoute,
-  teamsRoute,
-  teamSquadsRoute,
-  teamSquadRoute,
-  teamStatsRoute,
-  rootRoute,
-} = require('./routes')
-const {
-  playerAlbumSchema,
-  playerSchema,
-  playerStatsSchema,
-  playersSchema,
-  seasonHatTricksSchema,
-  seasonsSchema,
-  seasonTableSchema,
-  seasonSchema,
-  seasonTopAssistsSchema,
-  seasonTopScorersSchema,
-  teamSchema,
-  teamsSchema,
-  teamStatsSchema,
-  teamSquadsSchema,
-  teamSquadSchema,
-  rootSchema,
-} = require('./schemas')
+const router = require('./router')
 
 const app = fastify({
   logger: true,
@@ -57,47 +22,7 @@ app.register(swagger, {
   },
 })
 
-app.after(() => {
-  app.get('/', { schema: rootSchema }, rootRoute)
-
-  app.get('/players', { schema: playersSchema }, playersRoute)
-
-  app.get('/players/:playerId', { schema: playerSchema }, playerRoute)
-
-  app.get('/players/:playerId/album', { schema: playerAlbumSchema }, playerAlbumRoute)
-
-  app.get('/players/:playerId/stats', { schema: playerStatsSchema }, playerStatsRoute)
-
-  app.get('/teams', { schema: teamsSchema }, teamsRoute)
-
-  app.get('/teams/:teamId', { schema: teamSchema }, teamRoute)
-
-  app.get('/teams/:teamId/squads', { schema: teamSquadsSchema }, teamSquadsRoute)
-
-  app.get('/teams/:teamId/squads/:seasonId', { schema: teamSquadSchema }, teamSquadRoute)
-
-  app.get('/teams/:teamId/stats', { schema: teamStatsSchema }, teamStatsRoute)
-
-  app.get('/seasons', { schema: seasonsSchema }, seasonsRoute)
-
-  app.get('/seasons/:seasonId', { schema: seasonSchema }, seasonRoute)
-
-  app.get('/seasons/:seasonId/hat-tricks', { schema: seasonHatTricksSchema }, seasonHatTricksRoute)
-
-  app.get('/seasons/:seasonId/table', { schema: seasonTableSchema }, seasonTableRoute)
-
-  app.get(
-    '/seasons/:seasonId/top-assists',
-    { schema: seasonTopAssistsSchema },
-    seasonTopAssistsRoute
-  )
-
-  app.get(
-    '/seasons/:seasonId/top-scorers',
-    { schema: seasonTopScorersSchema },
-    seasonTopScorersRoute
-  )
-})
+app.register(router)
 
 app.get('/swagger', (_, response) => {
   response.send(app.swagger())
---
+++ server/router.js
@@ -0,0 +1,80 @@
+const {
+  playerRoute,
+  playersRoute,
+  playerAlbumRoute,
+  playerStatsRoute,
+  seasonsRoute,
+  seasonRoute,
+  seasonHatTricksRoute,
+  seasonTableRoute,
+  seasonTopAssistsRoute,
+  seasonTopScorersRoute,
+  teamRoute,
+  teamsRoute,
+  teamSquadsRoute,
+  teamSquadRoute,
+  teamStatsRoute,
+  rootRoute,
+} = require('./routes')
+const {
+  playerAlbumSchema,
+  playerSchema,
+  playerStatsSchema,
+  playersSchema,
+  seasonHatTricksSchema,
+  seasonsSchema,
+  seasonTableSchema,
+  seasonSchema,
+  seasonTopAssistsSchema,
+  seasonTopScorersSchema,
+  teamSchema,
+  teamsSchema,
+  teamStatsSchema,
+  teamSquadsSchema,
+  teamSquadSchema,
+  rootSchema,
+} = require('./schemas')
+
+module.exports = function router(app) {
+  app.get('/', { schema: rootSchema }, rootRoute)
+
+  app.get('/players', { schema: playersSchema }, playersRoute)
+
+  app.get('/players/:playerId', { schema: playerSchema }, playerRoute)
+
+  app.get('/players/:playerId/album', { schema: playerAlbumSchema }, playerAlbumRoute)
+
+  app.get('/players/:playerId/stats', { schema: playerStatsSchema }, playerStatsRoute)
+
+  app.get('/teams', { schema: teamsSchema }, teamsRoute)
+
+  app.get('/teams/:teamId', { schema: teamSchema }, teamRoute)
+
+  app.get('/teams/:teamId/squads', { schema: teamSquadsSchema }, teamSquadsRoute)
+
+  app.get('/teams/:teamId/squads/:seasonId', { schema: teamSquadSchema }, teamSquadRoute)
+
+  app.get('/teams/:teamId/stats', { schema: teamStatsSchema }, teamStatsRoute)
+
+  app.get('/seasons', { schema: seasonsSchema }, seasonsRoute)
+
+  app.get('/seasons/:seasonId', { schema: seasonSchema }, seasonRoute)
+
+  app.get('/seasons/:seasonId/hat-tricks', { schema: seasonHatTricksSchema }, seasonHatTricksRoute)
+
+  app.get('/seasons/:seasonId/table', { schema: seasonTableSchema }, seasonTableRoute)
+
+  app.get(
+    '/seasons/:seasonId/top-assists',
+    { schema: seasonTopAssistsSchema },
+    seasonTopAssistsRoute
+  )
+
+  app.get(
+    '/seasons/:seasonId/top-scorers',
+    { schema: seasonTopScorersSchema },
+    seasonTopScorersRoute
+  )
+
+  return app
+}
