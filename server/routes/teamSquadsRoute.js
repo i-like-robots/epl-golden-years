e418ba1825d673b67d6006c420e893c92b21859a
Matt Hinchliffe <matt@maketea.co.uk>
2023-07-26 09:34:20 +0100

    Refactor team squads data fetching into separate model module

--- server/routes/teamSquadsRoute.js
+++ server/routes/teamSquadsRoute.js
@@ -1,18 +1,12 @@
-const { squads } = require('../dataset')
+const teamSquadsModel = require('../resources/teamSquads/model')
 const { teamSquadUrl, teamUrl } = require('../lib/urls')
 
 module.exports = function teamSquadsRoute(request, response) {
   const { teamId } = request.params
+  const squads = teamSquadsModel(teamId)
 
-  const data = []
-
-  squads.forEach((squad) => {
-    if (squad.teamId === teamId) {
-      data.push(teamSquadUrl(teamId, squad.seasonId))
-    }
-  })
-
-  if (data.length) {
+  if (squads.length) {
+    const data = squads.map(({ seasonId }) => teamSquadUrl(teamId, seasonId))
     response.send({ team: teamUrl(teamId), squads: data })
   } else {
     response.code(404)
