1b4decbc3d5bf9515d92342c902722cfc3e401f4
Matt Hinchliffe <matt@maketea.co.uk>
2023-07-28 20:40:46 +0100

    Add managers graphql schema and query

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,10 +1,12 @@
 const { GraphQLObjectType, GraphQLSchema } = require('graphql')
 const { managerQuery } = require('./manager')
+const { managersQuery } = require('./managersQuery')
 
 const rootQuery = new GraphQLObjectType({
   name: 'Root',
   fields: () => ({
     ...managerQuery,
+    ...managersQuery,
   }),
 })
 
