6b1bd0d91acdfd92d5247088bbc64ac1d67851c7
Matt Hinchliffe <matt@maketea.co.uk>
2023-07-30 15:06:53 +0100

    Merge graphql schema modules into single file

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,19 +1,3 @@
-const { GraphQLObjectType, GraphQLSchema } = require('graphql')
-const { managerQuery } = require('./managerQuery')
-const { managersQuery } = require('./managersQuery')
-const { playerQuery } = require('./playerQuery')
-
-const rootQuery = new GraphQLObjectType({
-  name: 'Root',
-  fields: {
-    ...managerQuery,
-    ...managersQuery,
-    ...playerQuery,
-  },
-})
-
-const schema = new GraphQLSchema({
-  query: rootQuery,
-})
+const schema = require('./schema')
 
 module.exports = { schema }
---
+++ server/graphql/schema.js
@@ -0,0 +1,218 @@
+const {
+  GraphQLEnumType,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} = require('graphql')
+const { POSITION_CODES, POSITION_NAMES, PLAYER_ID } = require('../lib/constants')
+const { arrayToEnum, validateArg } = require('./utils')
+const managerModel = require('../models/managerModel')
+const managersModel = require('../models/managersModel')
+const playerAlbumModel = require('../models/playerAlbumModel')
+const playerHistoryModel = require('../models/playerHistoryModel')
+const playerModel = require('../models/playerModel')
+const playersModel = require('../models/playersModel')
+
+const historyType = new GraphQLObjectType({
+  name: 'History',
+  fields: {
+    seasonId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    teamId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+})
+
+const managerType = new GraphQLObjectType({
+  name: 'Manager',
+  fields: {
+    pulseId: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    optaId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    displayName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    firstName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    lastName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    dateOfBirth: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'ISO 8601 date, e.g. 1954-05-10',
+    },
+    countryCode: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'ISO-3166-1 code, e.g. GB-ENG',
+    },
+    countryName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    history: {
+      type: new GraphQLList(historyType),
+    },
+  },
+})
+
+const managersType = new GraphQLObjectType({
+  name: 'Managers',
+  fields: () => ({
+    managerId: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: (source) => source,
+    },
+  }),
+})
+
+const positionCodeType = new GraphQLEnumType({
+  name: 'PositionCode',
+  values: arrayToEnum(POSITION_CODES),
+})
+
+const positionNameType = new GraphQLEnumType({
+  name: 'PositionName',
+  values: arrayToEnum(POSITION_NAMES),
+})
+
+const albumType = new GraphQLObjectType({
+  name: 'Album',
+  fields: {
+    teamId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    seasonId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    sticker: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+})
+
+const playerType = new GraphQLObjectType({
+  name: 'Player',
+  fields: {
+    pulseId: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    optaId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    positionCode: {
+      type: new GraphQLNonNull(positionCodeType),
+    },
+    positionName: {
+      type: new GraphQLNonNull(positionNameType),
+    },
+    displayName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    firstName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    lastName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    dateOfBirth: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'ISO 8601 date, e.g. 1954-05-10',
+    },
+    countryCode: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'ISO-3166-1 code, e.g. GB-ENG',
+    },
+    countryName: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    history: {
+      type: new GraphQLList(historyType),
+      resolve: (source) => playerHistoryModel(source.playerId),
+    },
+    album: {
+      type: new GraphQLList(albumType),
+      resolve: (source) => playerAlbumModel(source.playerId),
+    },
+  },
+})
+
+const playersType = new GraphQLObjectType({
+  name: 'Players',
+  fields: () => ({
+    playerId: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: (source) => source,
+    },
+  }),
+})
+
+const rootQuery = new GraphQLObjectType({
+  name: 'Root',
+  fields: {
+    manager: {
+      type: managerType,
+      args: {
+        managerId: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_, { managerId }) => {
+        if (validateArg(managerId, PLAYER_ID, 'managerId')) {
+          return { managerId, ...managerModel(managerId) }
+        }
+      },
+    },
+    managers: {
+      type: new GraphQLList(managersType),
+      args: {
+        name: {
+          type: GraphQLString,
+        },
+      },
+      resolve: (_, { name }) => {
+        return managersModel({ name })
+      },
+    },
+    player: {
+      type: playerType,
+      args: {
+        playerId: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_, { playerId }) => {
+        if (validateArg(playerId, PLAYER_ID, 'playerId')) {
+          return { playerId, ...playerModel(playerId) }
+        }
+      },
+    },
+    players: {
+      type: new GraphQLList(playersType),
+      args: {
+        name: {
+          type: GraphQLString,
+        },
+        position: {
+          type: positionCodeType,
+        },
+      },
+      resolve: (_, { name, position }) => {
+        return playersModel({ name, position })
+      },
+    },
+  },
+})
+
+const schema = new GraphQLSchema({
+  query: rootQuery,
+})
+
+module.exports = schema
