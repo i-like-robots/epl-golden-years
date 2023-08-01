652384adff9b7827f351150c7ea5046f8e5b8d13
Matt Hinchliffe <matt@maketea.co.uk>
2023-08-01 20:04:58 +0100

    Add support for fetching gql schema via options request

--- server/routers/graphql.js
+++ server/routers/graphql.js
@@ -3,13 +3,12 @@ const { createHandler } = require('graphql-http/lib/use/fastify')
 const { schema } = require('../graphql')
 
 module.exports = function graphqlRouter(app, opts, done) {
-  app.all('/', createHandler({ schema }))
-
   const gql = printSchema(schema)
 
-  app.get('/schema.gql', (_, response) => {
-    response.send(gql)
-  })
+  app.get('/', createHandler({ schema }))
+  app.post('/', createHandler({ schema }))
+
+  app.options('/', (_, response) => response.send(gql))
 
   done()
 }
