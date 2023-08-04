94d57ffbdd9159464e157560c9231a0f18c2d265
Matt Hinchliffe <matt@maketea.co.uk>
2023-08-04 07:16:38 +0100

    Create a signle graphQL route handler instance

--- server/routers/graphql.js
+++ server/routers/graphql.js
@@ -2,11 +2,12 @@ const { printSchema } = require('graphql')
 const { createHandler } = require('graphql-http/lib/use/fastify')
 const { schema } = require('../graphql')
 
-module.exports = function graphqlRouter(app, opts, done) {
+module.exports = function graphqlRouter(app, _, done) {
   const gql = printSchema(schema)
+  const handler = createHandler({ schema })
 
-  app.get('/', createHandler({ schema }))
-  app.post('/', createHandler({ schema }))
+  app.get('/', handler)
+  app.post('/', handler)
 
   app.options('/', (_, response) => response.send(gql))
 
