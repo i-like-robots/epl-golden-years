db80f397e6869fb9bfc94aa14f5ebd2d43cee12c
Matt Hinchliffe <matt@maketea.co.uk>
2023-08-05 17:44:43 +0100

    Refactor top assists model to merge players in multiple squads

--- server/models/seasonTopAssistsModel.js
+++ server/models/seasonTopAssistsModel.js
@@ -10,19 +10,31 @@ function sortByAssists(a, b) {
 }
 
 module.exports = function seasonTopAssistsModel(seasonId) {
-  const draft = []
+  const unique = {}
 
   squads.forEach((squad) => {
-    if (squad.seasonId === seasonId) {
-      squad.players.forEach((player) => {
-        if (player.assists) {
-          draft.push(player)
-        }
-      })
+    if (squad.seasonId !== seasonId) {
+      return
     }
+
+    squad.players.forEach((player) => {
+      if (player.assists === 0) {
+        return
+      }
+
+      unique[player.playerId] ??= {
+        playerId: player.playerId,
+        assists: 0,
+        appearances: 0,
+      }
+
+      unique[player.playerId].assists += player.assists
+      unique[player.playerId].appearances += player.appearances
+    })
   })
 
-  // TODO: merge players if needed, i.e. they transferred
+  const draft = Array.from(Object.values(unique))
+
   draft.sort(sortByAssists)
 
   return draft.slice(0, 10).map((player) => ({
