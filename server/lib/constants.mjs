cc8aac53d19414b8a628d882d584294a44a273d3
Matt Hinchliffe <matt@maketea.co.uk>
2023-10-10 20:08:25 +0100

    Convert library modules to ESM

---
+++ server/lib/constants.mjs
@@ -0,0 +1,11 @@
+export const API_URL = process.env.BASE_URL || 'http://localhost:3000/'
+
+export const PERSON_ID = '^(?:[a-z]+-)+[a-z0-9]{4}$'
+
+export const POSITION_CODES = ['G', 'D', 'M', 'F']
+
+export const POSITION_NAMES = ['Goalkeeper', 'Defender', 'Midfielder', 'Winger', 'Forward']
+
+export const SEASON_ID = '^\\d{4}-\\d{4}$'
+
+export const TEAM_ID = '^[a-z]{3}$'
--- server/lib/object-get.mjs
+++ server/lib/object-get.mjs
@@ -1,4 +1,4 @@
-module.exports = function objectGet(obj, prop) {
+export default function objectGet(obj, prop) {
   const exists = Object.prototype.hasOwnProperty.call(obj, prop)
   return exists ? obj[prop] : null
 }
