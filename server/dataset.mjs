04cd70e08e41c6aa4a7ce0834cf2d984b72d1be0
Matt Hinchliffe <matt@maketea.co.uk>
2023-10-10 20:38:10 +0100

    Convert JSON dataset module to ESM

---
+++ server/dataset.mjs
@@ -0,0 +1,8 @@
+export { default as hatTricks } from '../data/hat-tricks.json' assert { type: 'json' }
+export { default as managers } from '../data/managers.json' assert { type: 'json' }
+export { default as players } from '../data/players.json' assert { type: 'json' }
+export { default as seasons } from '../data/seasons.json' assert { type: 'json' }
+export { default as stickers } from '../data/laststicker.json' assert { type: 'json' }
+export { default as squads } from '../data/squads.json' assert { type: 'json' }
+export { default as tables } from '../data/tables.json' assert { type: 'json' }
+export { default as teams } from '../data/teams.json' assert { type: 'json' }
