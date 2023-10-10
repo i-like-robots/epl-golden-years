43821df4c3fbda666730955c9f024a5cd63758b2
Matt Hinchliffe <matt@maketea.co.uk>
2023-10-10 21:33:58 +0100

    Refactor dataset module to not use experimental import assertions

--- server/dataset.mjs
+++ server/dataset.mjs
@@ -1,8 +1,23 @@
-export { default as hatTricks } from '../data/hat-tricks.json' assert { type: 'json' }
-export { default as managers } from '../data/managers.json' assert { type: 'json' }
-export { default as players } from '../data/players.json' assert { type: 'json' }
-export { default as seasons } from '../data/seasons.json' assert { type: 'json' }
-export { default as stickers } from '../data/laststicker.json' assert { type: 'json' }
-export { default as squads } from '../data/squads.json' assert { type: 'json' }
-export { default as tables } from '../data/tables.json' assert { type: 'json' }
-export { default as teams } from '../data/teams.json' assert { type: 'json' }
+import fs from 'fs'
+import url from 'url'
+import path from 'path'
+
+function resolveFile(fileName) {
+  const dirname = url.fileURLToPath(path.dirname(import.meta.url))
+  return path.join(dirname, '..', 'data', fileName)
+}
+
+function loadJSON(fileName) {
+  const fullPath = resolveFile(fileName)
+  const contents = fs.readFileSync(fullPath, 'utf-8')
+  return JSON.parse(contents)
+}
+
+export const hatTricks = loadJSON('hat-tricks.json')
+export const managers = loadJSON('managers.json')
+export const players = loadJSON('players.json')
+export const seasons = loadJSON('seasons.json')
+export const stickers = loadJSON('laststicker.json')
+export const squads = loadJSON('squads.json')
+export const tables = loadJSON('tables.json')
+export const teams = loadJSON('teams.json')
