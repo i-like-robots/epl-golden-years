f1e8471880131e803dda8a14d975e4ca4a7a6d68
1988cp <1988cpaul@gmail.com>
2024-07-24 21:07:42 +0100

    Add new React App and dependencies

---
+++ test-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <body style={{backgroundImage: "url(./images/pitch.jpg)"}}>
+      <h2>up the footy</h2>
+      </body>
+      
+    </div>
+  );
+}
+
+export default App;
